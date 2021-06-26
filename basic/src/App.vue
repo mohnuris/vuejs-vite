64fc0bd2fafb84c7b062e59c558e4c8ea5a0599e
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2021-06-27 04:43:17 +0700

    decrlarative rendering

---
+++ basic/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <div>{{ count }}</div>
+  
+  <button @click="count++">Add 1</button>
+</template>
+
+<script>
+export default {
+data() {
+  return {
+    count: 0
+  }
+}
+}
+</script>
